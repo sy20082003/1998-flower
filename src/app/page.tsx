17dcd108b25f56e88d5c8179752ed8fda19d4c27
sy20082003 <sy20082003@gmail.com>
2026-03-26 02:50:17 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,12 +131,14 @@ const HomePage = () => {
       <section className="relative h-[65vh] md:h-[80vh] lg:h-[90vh] flex items-center overflow-hidden">
         <div className="absolute inset-0 z-0">
           <Image
-  src="/image/hero-image.jpg"
-  alt="Hero floral background"
-  fill
-  priority
-  className="object-cover brightness-[0.8]"
-/>
+            width={1920}
+            height={800}
+            src="image/hero-image.jpg"
+            alt="Hero floral background" 
+            fill
+            priority
+            className="w-full h-full object-cover brightness-[0.8]"
+          />
           <div className="absolute inset-0 bg-linear-to-r from-black/40 via-black/10 to-transparent" />
         </div>
 
